5c4675169099f4e39fc1deaf0755bfec34d6f76b
Aditi-Podder-1213 <aditipodder2005@gmail.com>
2026-09-06 13:05:12 +0600

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,16 @@
+const students = ["Bill Gates", "Steve Jobs", "Mark Zuckerberg"];
+
+console.log(students[0]);
+console.log(students[1]);
+console.log(students.length);
+
+
+students.push("Elon Musk");
+
+students.pop();
+
+students.unshift("Elon Musk");
+
+students.shift();
+
+console.log(students);
