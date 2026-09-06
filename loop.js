5f413cbbe71f7d91433fff1508fe1cf8f251d50e
Aditi-Podder-1213 <aditipodder2005@gmail.com>
2026-09-06 13:14:09 +0600

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,21 @@
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
+
+const students = ["Bill Gates", "Steve Jobs", "Mark Zuckerberg"];
+
+for (const student of students) {
+  console.log(student);
+}
